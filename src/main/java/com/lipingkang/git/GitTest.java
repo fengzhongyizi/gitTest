25c6ea2f1c2589b1b9621618bc6370c5a519e064
fengzhongyizi <fengzhongyizi@icloud.com>
2022-11-25 00:22:34 +0800

    First Version

---
+++ src/main/java/com/lipingkang/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.lipingkang.git;
+
+/**
+ * @author Administrator
+ * @date 2022/11/25
+ * @project javaSE
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
