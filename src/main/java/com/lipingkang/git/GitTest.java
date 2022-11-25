affe4dd4b6f86946771927b5afc286cb80da917e
fengzhongyizi <fengzhongyizi@icloud.com>
2022-11-25 11:13:53 +0800

    sixth Version

--- src/main/java/com/lipingkang/git/GitTest.java
+++ src/main/java/com/lipingkang/git/GitTest.java
@@ -7,10 +7,11 @@ package com.lipingkang.git;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("Hello Git");//first version
+        System.out.println("Hello Git");//first version-->add
         System.out.println("modify");//second version-->直接提交 未添加暂存区 提交信息 Second Version
         System.out.println("modify");//third version-->直接提交 未添加暂存区 提交信息 Third Version
         System.out.println("modify and add");//fourth version-->add
         System.out.println("modify and add");//fifth version-->add
+        System.out.println("modify");//sixth version-->直接提交 未添加暂存区
     }
 }
