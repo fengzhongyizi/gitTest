f3b75e8f1b70cc71b61b3406529493a18459e7fb
fengzhongyizi <fengzhongyizi@icloud.com>
2022-11-25 11:12:40 +0800

    fifth Version

--- src/main/java/com/lipingkang/git/GitTest.java
+++ src/main/java/com/lipingkang/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("modify");//second version-->直接提交 未添加暂存区 提交信息 Second Version
         System.out.println("modify");//third version-->直接提交 未添加暂存区 提交信息 Third Version
         System.out.println("modify and add");//fourth version-->add
+        System.out.println("modify and add");//fifth version-->add
     }
 }
