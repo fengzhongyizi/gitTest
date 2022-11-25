cd1fc56691408fbbbfe9a8e45e6ad486af73409f
fengzhongyizi <fengzhongyizi@icloud.com>
2022-11-25 11:14:58 +0800

    seventh Version

--- src/main/java/com/lipingkang/git/GitTest.java
+++ src/main/java/com/lipingkang/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("modify and add");//fourth version-->add
         System.out.println("modify and add");//fifth version-->add
         System.out.println("modify");//sixth version-->直接提交 未添加暂存区
+        System.out.println("modyfy");//seventh version-->直接提交 未添加暂存区
     }
 }
