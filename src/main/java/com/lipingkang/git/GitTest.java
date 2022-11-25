210cb523a0f3a9e11dda6c04263161ea385d76fd
fengzhongyizi <46082773+fengzhongyizi@users.noreply.github.com>
2022-11-25 15:41:36 +0800

    pull test

--- src/main/java/com/lipingkang/git/GitTest.java
+++ src/main/java/com/lipingkang/git/GitTest.java
@@ -18,5 +18,7 @@ public class GitTest {
         System.out.println("conflict test master");
         System.out.println("comflict test hotfix");
         System.out.println("try SSH");
+        System.out.println("try Pull");
+
     }
 }
